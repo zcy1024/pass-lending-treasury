78f05b9925957aee7d7fead85e1db06451762005
Zcy <zcy_chao1024@163.com>
2025-08-15 17:11:33 +0800

    show updated coins info

--- src/components/coins/coinInfo.tsx
+++ src/components/coins/coinInfo.tsx
@@ -3,24 +3,43 @@
 import { Button } from "@/components/ui/button";
 import { useAppSelector, AppDispatch } from "@/store";
 import { useDispatch } from "react-redux";
-import { setTransferList } from "@/store/modules/info";
+import { coinType, setTransferList } from "@/store/modules/info";
+import { useEffect, useState } from "react";
 
 export default function CoinInfo() {
     const dispatch = useDispatch<AppDispatch>();
     const coins = useAppSelector(state => state.info.coins);
+    const newCoins = useAppSelector(state => state.info.newCoins);
     const transferList = useAppSelector(state => state.info.transferList);
+
+    const [infos, setInfos] = useState<coinType[]>([]);
+    useEffect(() => {
+        let temp: coinType[] = [];
+        temp = temp.concat(coins.map(coin => {
+            const index = newCoins.findIndex(newCoin => coin.name === newCoin.name);
+            return index === -1 ? coin : newCoins[index];
+        }))
+        temp = temp.concat(newCoins.filter(newCoin => coins.findIndex(coin => coin.name === newCoin.name) === -1));
+        setInfos(temp.filter(coin => coin.value > 0));
+    }, [coins, newCoins]);
+
+    const checkUpdated = (newCoin: coinType) => {
+        const index = coins.findIndex(coin => coin.name === newCoin.name);
+        return index === -1 || coins[index].value !== newCoin.value;
+    }
 
     return (
         <div className="flex flex-col gap-3 items-center h-full w-1/12 min-w-[150px] font-mono">
             <span className="font-bold text-xl">Coins</span>
-            {coins.map((info, index) => {
+            {infos.length === 0 && <span className="font-sans text-xs text-[#afb3b5]">no coins</span>}
+            {infos.map((info, index) => {
                 const canTransfer = transferList.find(list => list.name === info.name) === undefined;
                 return (
                     <div key={index}
                          className={"flex flex-col gap-1 w-full rounded-xl p-1 group " + (index % 2 === 0 ? "bg-[#F9F9F9]" : "bg-[#FFF]")}>
                         <div className="flex justify-between items-center">
                             <span>{info.name}:</span>
-                            <span>{info.value}</span>
+                            <span className={checkUpdated(info) ? "text-[#35a1f7]" : ""}>{info.value}</span>
                         </div>
                         {
                             canTransfer &&
@@ -37,8 +56,8 @@ export default function CoinInfo() {
                     </div>
                 );
             })}
-            <Button className="w-full cursor-pointer" disabled={coins.length === transferList.length}
-                    onClick={() => dispatch(setTransferList(coins.map(coin => {
+            <Button className="w-full cursor-pointer" disabled={infos.length === transferList.length}
+                    onClick={() => dispatch(setTransferList(infos.map(coin => {
                         return {
                             ...coin,
                             transferValue: coin.value.toString()
