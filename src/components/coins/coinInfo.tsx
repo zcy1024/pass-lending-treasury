3e8eaea6744beed8b55b135bb5c0485761bf31db
Zcy <zcy_chao1024@163.com>
2025-08-26 17:41:41 +0800

    use scroll in coin info list

--- src/components/coins/coinInfo.tsx
+++ src/components/coins/coinInfo.tsx
@@ -3,6 +3,7 @@
 import { useAppSelector } from "@/store";
 import { coinType } from "@/store/modules/info";
 import { Swap, TransferCard } from "@/components";
+import { ScrollArea } from "@/components/ui/scroll-area";
 
 export default function CoinInfo() {
     const coins = useAppSelector(state => state.info.coins);
@@ -17,17 +18,21 @@ export default function CoinInfo() {
         <div className="flex flex-col gap-3 items-center h-full w-1/12 min-w-[150px] font-mono">
             <span className="font-bold text-xl">Coins</span>
             {realCoins.length === 0 && <span className="font-sans text-xs text-[#afb3b5]">no coins</span>}
-            {realCoins.map((info, index) => {
-                return (
-                    <div key={index}
-                         className={"flex flex-col gap-1 w-full rounded-xl p-1 group " + (index % 2 === 0 ? "bg-[#F9F9F9]" : "bg-[#FFF]")}>
-                        <div className="flex justify-between items-center">
-                            <span>{info.name}:</span>
-                            <span className={checkUpdated(info) ? "text-[#35a1f7]" : ""}>{(info.value / info.decimals).toFixed(2)}</span>
-                        </div>
-                    </div>
-                );
-            })}
+            <ScrollArea className="w-full h-full">
+                <div className="flex flex-col gap-3 w-full max-h-96">
+                    {realCoins.map((info, index) => {
+                        return (
+                            <div key={index}
+                                 className={"w-full rounded-xl p-1 group " + (index % 2 === 0 ? "bg-[#F9F9F9]" : "bg-[#FFF]")}>
+                                <div className="flex justify-between items-center">
+                                    <span>{info.name}:</span>
+                                    <span className={checkUpdated(info) ? "text-[#35a1f7]" : ""}>{(info.value / info.decimals).toFixed(2)}</span>
+                                </div>
+                            </div>
+                        );
+                    })}
+                </div>
+            </ScrollArea>
             <TransferCard />
             <Swap />
         </div>
