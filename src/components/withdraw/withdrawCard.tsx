432311a8d2f1d3f620fc70159d1f58dd955eb3bb
Zcy <zcy_chao1024@163.com>
2025-08-27 10:23:06 +0800

    withdraw card layout

--- src/components/withdraw/withdrawCard.tsx
+++ src/components/withdraw/withdrawCard.tsx
@@ -6,6 +6,8 @@ import { Button } from "@/components/ui/button";
 import { RewardsDetail } from "@/components";
 import { useState } from "react";
 import { useAppSelector } from "@/store";
+import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar";
+import { Check } from "lucide-react";
 
 export default function WithdrawCard({title, withdrawCoins, rewardCoins}: {
     title: string,
@@ -17,6 +19,7 @@ export default function WithdrawCard({title, withdrawCoins, rewardCoins}: {
         coinType: string,
         amount: string
     }[]>([]);
+    const [isValid, setIsValid] = useState<boolean>(true);
 
     const getWithdrawAmount = (type: string) => {
         const index = withdrawInfos.findIndex(info => info.coinType === type);
@@ -36,7 +39,12 @@ export default function WithdrawCard({title, withdrawCoins, rewardCoins}: {
     const isInvalidWithdrawAmount = (type: string) => {
         const input = getWithdrawAmount(type);
         const index = withdrawCoins.findIndex(coin => coin.coinType === type);
-        return !input || index === -1 || Number(input) === 0 || Number(input) > withdrawCoins[index].supplied;
+        const coinIndex = coins.findIndex(coin => coin.coinType === type);
+        return !input || index === -1 || Number(input) === 0 || Number(input) * coins[coinIndex].decimals > withdrawCoins[index].supplied;
+    }
+
+    const addTransaction = async () => {
+        console.log("withdraw");
     }
 
     return (
@@ -49,7 +57,13 @@ export default function WithdrawCard({title, withdrawCoins, rewardCoins}: {
 
                 return (
                     <div key={index} className="flex justify-between items-center px-10 m-1">
-                        <h4 className="font-bold">{coin.name}</h4>
+                        <div className="flex gap-1 items-center">
+                            <Avatar className="h-6 w-6">
+                                <AvatarImage src={coin.src} alt={coin.alt} />
+                                <AvatarFallback>{coin.fallback}</AvatarFallback>
+                            </Avatar>
+                            <h4 className="font-bold">{coin.name}</h4>
+                        </div>
                         <div className="flex gap-10 items-center">
                             <span className="text-xs text-[#afb3b5]">Supplied: {(coin.supplied / decimals).toFixed(2)}</span>
                             <div className="flex gap-2 items-center">
@@ -68,16 +82,22 @@ export default function WithdrawCard({title, withdrawCoins, rewardCoins}: {
                                         onClick={() => updateWithdrawInfo(coin.coinType, (coin.supplied / decimals).toString())}>
                                     Max
                                 </Button>
-                                <Button className="w-24 h-6 cursor-pointer font-sans"
-                                        disabled={isInvalidWithdrawAmount(coin.coinType)}>
-                                    Withdraw
-                                </Button>
+                                <Check color="green" size={36}
+                                       className={"transition-opacity " + (isInvalidWithdrawAmount(coin.coinType) ? "opacity-0" : "opacity-100")} />
                             </div>
                         </div>
                     </div>
                 );
             })}
-            <div className="flex gap-2 justify-end items-center mt-6">
+            <div className="flex flex-row-reverse gap-3 items-center px-5 mt-2">
+                <Button className="w-36 h-8 cursor-pointer font-sans"
+                        disabled={!withdrawCoins.find(coin => !isInvalidWithdrawAmount(coin.coinType))}
+                        onClick={addTransaction}>
+                    Withdraw
+                </Button>
+                <span className="text-xs font-sans text-red-600">{isValid ? "" : "error withdraw"}</span>
+            </div>
+            <div className="flex gap-3 justify-end items-center px-5 mt-2">
                 <RewardsDetail title={title} rewardCoins={rewardCoins} />
                 <Button className="w-36 h-8 cursor-pointer font-sans">Claim Reward</Button>
                 <Button className="w-36 h-8 cursor-pointer font-sans">Claim And ReSupply</Button>
