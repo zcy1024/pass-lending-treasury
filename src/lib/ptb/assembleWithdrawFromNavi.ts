6701cee7826e91be4699e3dea33deb7889826172
Zcy <zcy_chao1024@163.com>
2025-08-27 15:05:03 +0800

    merge to withdraw(avoid contract's error)

--- src/lib/ptb/assembleWithdrawFromNavi.ts
+++ src/lib/ptb/assembleWithdrawFromNavi.ts
@@ -5,9 +5,16 @@ import { extraCoinType } from "@/lib/ptb/index";
 
 export default async function assembleWithdrawFromNavi(tx: Transaction, transaction: withdrawFromNaviType) {
     const extraCoins: extraCoinType[] = [];
+    const usedTypes: string[] = [];
     for (const i in transaction.coinTypes) {
         const type = transaction.coinTypes[i];
-        const amount = Math.floor(transaction.values[i]);
+        if (usedTypes.findIndex(usedType => usedType === type) !== -1)
+            continue;
+        usedTypes.push(type);
+        let amount = 0;
+        for (let j = Number(i); j < transaction.coinTypes.length; j++)
+            if (transaction.coinTypes[j] === type)
+                amount += Math.floor(transaction.values[j]);
         extraCoins.push({
             // @ts-expect-error #private transaction
             coin: await withdrawCoinPTB(tx, type, amount),
