7ea33090c5a135e3cbf63957117cf4b10ebe7bb2
Zcy <zcy_chao1024@163.com>
2025-09-30 15:38:16 +0800

    sui client publish

--- src/configs/networkConfig.ts
+++ src/configs/networkConfig.ts
@@ -17,6 +17,12 @@ const {networkConfig, useNetworkVariable, useNetworkVariables} = createNetworkCo
                 Package: "0x83bbe0b3985c5e3857803e2678899b03f3c4a31be75006ab03faf268c014ce41",
                 Version: "0x07871c4b3c847a0f674510d4978d5cf6f960452795e8ff6f189fd2088a3f6ac7",
                 Market: "0xa757975255146dc9686aa823b7838b507f315d704f428cbadad2f4ea061939d9"
+            },
+            Leaderboard: {
+                Package: "0xb9d5afbe7328e92356674ef33fa0a2b6493691354ecf74015cc3f290dc2238e6",
+                UpgradeCap: "0x455f72e223309566d314fc2b46cab4329182bc8b787fcd73a2cc7c916e85e8ea",
+                AdminCap: "0xa5207acd6b5ce43dc12736202d8f6a70ea3f0adc09ca456dd0a15256bc2c93e6",
+                InfoList: "0x13471596227ffe723a15ba0de4471d11ed9e14aabc1d4c19887fc60d8554077d"
             }
         }
     }
